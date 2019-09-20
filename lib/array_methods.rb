62cc5a63c9eaf83193ae73510ea865ee146ce175
Tommy Connor <tconnor98@aol.com>
2019-09-20 20:57:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   (find_element_index(scale,2)).to eq(0)
-      expect(find_element_index(scale,4)).to eq(2)
-      expect(find_element_index(scale_2,5)).to eq(1)
-      expect(find_element_index(scale_2,6)).to eq(0)
+  find_element_index(scale,4)).to eq(2)
+    find_element_index(scale_2,5)).to eq(1)
+     find_element_index(scale_2,6)).to eq(0)
 end
 
 def find_max_value(array)
